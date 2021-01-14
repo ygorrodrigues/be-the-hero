dd0b969489b19139919759480ff036c36a760e73
Ygor Telmo <ygor.telmo@hotmail.com>
2021-01-14 19:08:06 -0300

    Starting backend spring application

---
+++ backend/src/main/java/com/ygort/bethehero/BeTheHeroApplication.java
@@ -0,0 +1,13 @@
+package com.ygort.bethehero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeTheHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeTheHeroApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ygort/bethehero/BeTheHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygort.bethehero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeTheHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

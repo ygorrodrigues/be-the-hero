1643cd89775365e6aa9a4262888b93c4dbe6b3a5
Ygor Telmo <ygor.telmo@hotmail.com>
2021-01-20 21:08:29 -0300

    Added api calls in frontend at all points

--- backend/src/main/java/com/ygort/bethehero/entities/ONG.java
+++ backend/src/main/java/com/ygort/bethehero/entities/ONG.java
@@ -12,7 +12,7 @@ import java.util.Set;
 public class ONG implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
